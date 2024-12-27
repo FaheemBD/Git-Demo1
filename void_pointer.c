e7128d2203f704142ec7688550a8d64d8e9c76f6
FaheemBD <md.abedhasanfahim@gmail.com>
2024-12-28 01:56:48 +0600

    all the comments are removed from void_pointer.c file

--- void_pointer.c
+++ void_pointer.c
@@ -13,69 +13,9 @@ void print(struct point x){
 
 int main()
 {
-    /*
-
-    //void pointer
-    void *p;
-    printf("%d %d",(int) p,*(int *)p);
-
-    */
-
-    /*
-    //null pointer
-    int *a;
-    *a = NULL;
-    printf("%d",*a);
-    */
-
-    /*
-    //dangling pointer
-    int x = 10;
-    int *a = (int *)malloc(sizeof(int));
-
-    a = &x;
-    printf("%d %d\n",a,*a);
-
-    free(a);
-    *a = NULL;
-
-
-    printf("%d %d\n",a,*a);
-    */
-
-    //int *p = (int *)malloc(sizeof(int));
-    /* malloc , calloc realloc and free in use........
-    int *p = (int *)calloc(3,sizeof(int));
-    int *d = (int *)realloc(p,12*sizeof(int));
-
-    *d = 20;
-    *(d+1) = 30;
-    *(d+2) = 40;
-    *(d+3) = 90;
-    *(d+4) = 60;
-
-
-
-    printf("%d %d %d %d %d\n",*d,*(d+1),*(d+2),*(d+3),*(d+4));
-    printf("%d",sizeof(d));
-
-    free(p);
-    printf("%d %d %d %d %d\n",*d,*(d+1),*(d+2),*(d+3),*(d+4));
-    printf("%d",sizeof(d));
-    */
-
-    //passing structure members as arguments
 
     struct point p = {94,5};
     print(p);
-
-
-
-
-
-
-
-
 
     return 0;
 }
